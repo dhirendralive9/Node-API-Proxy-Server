6e700e623384c53c57e750428dd200af650bd511
Dhirendra Biswal <dhirendrabiswal9@live.com>
2022-04-06 23:29:20 +0530

    api caching done

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const url = require('url');
 const express = require('express');
 const router = express.Router();
 const needle = require('needle');
+const apicache = require('apicache')
 
 // Env variables 
 
@@ -9,8 +10,11 @@ const API_BASE_URL = process.env.API_BASE_URL;
 const API_KEY_NAME = process.env.API_KEY_NAME;
 const API_KEY_VALUE = process.env.API_KEY_VALUE;
 
+// /init cache
 
-router.get('/',async (req, res) => {
+let cache = apicache.middleware
+
+router.get('/',cache('2 minutes') ,async (req, res) => {
     
      try {
    
